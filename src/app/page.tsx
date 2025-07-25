a4cf324dc1773529fe63f6ca8caa0514dcfd6d98
Subhan Kashif <subhankashif696@gmail.com>
2025-07-25 16:48:29 +0500

    Set up Auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
+"use client"
+import { SignedIn, SignedOut, SignInButton } from '@clerk/clerk-react';
+import { SignOutButton } from '@clerk/nextjs';
 import React from 'react'
 
 const HomePage = () => {
   return (
     <div>
       HomePage
+
+      <SignedIn>
+      <SignOutButton />
+      </SignedIn>
+
+      <SignedOut>
+      <SignInButton />
+      </SignedOut>
+      
     </div>
   )
 }
