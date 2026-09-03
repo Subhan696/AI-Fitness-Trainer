1ac319d4e7d9bcf4cff796623ce07981b0471c6a
Subhan <subhankashif696@gmail.com>
2026-09-03 14:59:11 +0500

    Add Clerk proxy matcher to middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,11 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
 
-const isProtectedRoute= createRouteMatcher(["/generate-program","profile"]);
+const isProtectedRoute = createRouteMatcher(["/generate-program", "/profile"]);
 
-export default clerkMiddleware(async(auth,req)=>{
-    if (isProtectedRoute(req)) await auth.protect();
+export default clerkMiddleware(async (auth, req) => {
+  if (isProtectedRoute(req)) await auth.protect();
 });
-
 
 export const config = {
   matcher: [
@@ -14,6 +13,8 @@ export const config = {
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     // Always run for API routes
     '/(api|trpc)(.*)',
+    // Clerk Proxy
+    '/__clerk/:path*',
   ],
 };
 
